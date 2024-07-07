3fde4b9d2d222d3f46bfed44302da75c8cfeda68
木葉 Scalret <kevyn031119@outlook.com>
2024-07-07 20:23:50 +0800

    + 编辑时检查外键

--- src/components/base/EditDialog.svelte
+++ src/components/base/EditDialog.svelte
@@ -4,6 +4,8 @@
     import DateTimeInput from "./DateTimeInput.svelte";
 
     import type { Row, Columns } from "$/utils/db";
+    import { NetUtils } from "$/utils/net";
+    import { error } from "$/utils/alert";
 
     export let columns: Columns;
 
@@ -17,6 +19,23 @@
     export let style: string | undefined = undefined;
 
     export let handler: () => any;
+
+    const foreignKeyCheck = async () => {
+        if (!row) return false;
+
+        let flag = true;
+        for (const [key, { renderName, foreignKey }] of Object.entries(columns)) {
+            if (!foreignKey) continue;
+
+            await NetUtils.query(foreignKey!.tableName, {
+                searchBy: foreignKey!.key,
+                query: row[key],
+                match: "eq",
+            }).then((json) => json.data.count || ((flag = false), error(`外键 ${renderName} (${key}) 找不到对应值!`, 5000)));
+        }
+
+        return flag;
+    };
 </script>
 
 <!-- 通用编辑屏幕 -->
@@ -26,29 +45,54 @@
             {#each Object.entries(columns) as [key, column] (key)}
                 <div class={column.editRule === "textarea" ? "col-span-3 max-h-[50vh]" : ""}>
                     <label class="form-control">
-                        <span class="label-text" class:font-semibold={column.isPrimary} class:italic={column.foreignKey}>
-                            {column.renderName}
-                        </span>
-                        <!-- TODO: 外键特殊处理 -->
-                        <!-- 是否需要? -->
+                        <div class="label">
+                            <span class="label-text" class:font-semibold={column.isPrimary} class:italic={column.foreignKey}>
+                                {column.renderName}
+                            </span>
+                            {#if column.foreignKey}
+                                <span class="label-text-alt">{column.foreignKey.tableName} - {column.foreignKey.key}</span>
+                            {/if}
+                        </div>
                         {#if column.editRule === "number"}
                             <input
                                 type="number"
                                 step={column.step}
                                 inputmode="numeric"
                                 class="input input-bordered"
+                                class:input-primary={column.isPrimary}
+                                class:input-secondary={column.foreignKey}
                                 readonly={column.isImmutable}
                                 bind:value={row[key]}
                             />
                         {:else if column.editRule === "text"}
-                            <input type="text" class="input input-bordered" readonly={column.isImmutable} bind:value={row[key]} />
+                            <input
+                                type="text"
+                                class="input input-bordered"
+                                class:input-primary={column.isPrimary}
+                                class:input-secondary={column.foreignKey}
+                                readonly={column.isImmutable}
+                                bind:value={row[key]}
+                            />
                         {:else if column.editRule === "date"}
-                            <DateInput class="input input-bordered" readonly={column.isImmutable} bind:value={row[key]} />
+                            <DateInput
+                                class="input input-bordered{column.isPrimary ? ' input-primary' : ''}{column.foreignKey ? ' input-secondary' : ''}"
+                                readonly={column.isImmutable}
+                                bind:value={row[key]}
+                            />
                         {:else if column.editRule === "datetime"}
-                            <DateTimeInput class="input input-bordered" readonly={column.isImmutable} bind:value={row[key]} />
+                            <DateTimeInput
+                                class="input input-bordered{column.isPrimary ? ' input-primary' : ''}{column.foreignKey ? ' input-secondary' : ''}"
+                                readonly={column.isImmutable}
+                                bind:value={row[key]}
+                            />
                         {:else if column.editRule === "textarea"}
-                            <textarea class="textarea textarea-bordered textarea-md" readonly={column.isImmutable} bind:value={row[key]}></textarea>
-                            <!-- {:else} -->
+                            <textarea
+                                class="textarea textarea-bordered textarea-md"
+                                class:input-primary={column.isPrimary}
+                                class:input-secondary={column.foreignKey}
+                                readonly={column.isImmutable}
+                                bind:value={row[key]}
+                            ></textarea>
                         {/if}
                     </label>
                 </div>
@@ -58,7 +102,7 @@
 
     <div slot="action" class="modal-action items-center">
         <span class="text-info">该操作不可撤销</span>
-        <form method="dialog" on:submit={handler}>
+        <form method="dialog" on:submit={async () => ((await foreignKeyCheck()) ? handler() : error("外键不匹配, 请检查输出信息!", 5000))}>
             <button class="btn btn-info">确认</button>
         </form>
     </div>
